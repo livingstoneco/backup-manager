54d2d797802f468fe44a99f226d98f4266aabd8b
Neil Livingstone <neil@livingstone.co>
2020-06-01 10:53:14 -0400

    WIP: Obtain backup from off-site storage

--- app/Console/Commands/Restore.php
+++ app/Console/Commands/Restore.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 class Restore extends Command
 {
@@ -37,6 +40,25 @@ class Restore extends Command
      */
     public function handle()
     {
-        //
+        // Get a list of backup files
+        $backups = Storage::disk('s3')->allFiles('/backups');
+       
+        // Which back should we restore
+        $backup = $this->choice('Which backup would you like to restore?', $backups);
+
+        // Is the backup encrypted
+        $isEncrypted = $this->choice('The selected backup encrypted?', ['false','true']);
+
+        // Are you sure you want to proceed
+        if (!$this->confirm("Are you sure you want to restore backup {$backup}?")) {
+            $this->error("Please confirm you wish to import backup {$backup}");
+            // Lets try this again...
+            $this->call('restore');
+            exit();
+        }
+
+        // Get file contents
+        $sql = Storage::disk('s3')->get($backup);
+
     }
 }
